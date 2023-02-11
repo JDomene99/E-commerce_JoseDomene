ad1a3605f6abcaf86e5dac58914c66691d598f68
jdomene99 <jdomene99@gmail.com>
2023-02-11 10:49:18 +0100

    Finish

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,8 +13,6 @@ export const register = async (req, res) => {
         picturePath,
       } = req.body;
       
-      console.log(req.body);
-
       const salt = await bcrypt.genSalt();
       const passwordHash = await bcrypt.hash(password, salt);
       
