56cfee8cbb243e9d58ca0d67d4da8d43b542e990
jdomene99 <jdomene99@gmail.com>
2023-02-12 14:11:03 +0100

    adfa

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,117 +1,100 @@
 import express from "express";
-import Product from '../db/models/productModel.js';
+import Product from "../db/models/productModel.js";
 
+export const insertProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      stock,
+      img,
+      likes,
+      comments,
+      category,
+      price,
+      size,
+    } = req.body;
 
-export const insertProduct = async (req, res) =>{
-    
-    try {
-        
-        const {
-          name,
-          description,
-          stock,
-          img,
-          likes,
-          comments,
-          category,
-          price,
-          size
-        } = req.body; 
-        
-        const newProduct = new Product({
-          name,
-          description,
-          stock,
-          img,
-          likes,
-          comments,
-          category,
-          price,
-          size 
-        });
-        const savedProduct = await newProduct.save();
-        res.status(201).json(savedProduct);
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
-}
+    const newProduct = new Product({
+      name,
+      description,
+      stock,
+      img,
+      likes,
+      comments,
+      category,
+      price,
+      size,
+    });
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
-export const getAllProduct = async (req, res) =>{
-    try {
-        const data = await Product.find({});
-        res.send(data)
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    
-}
+export const getAllProduct = async (req, res) => {
+  try {
+    const data = await Product.find({});
+    res.send(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
+export const deleteProduct = async (req, res) => {
+  try {
+    const post = await Product.deleteOne({ _id: req.params.id });
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-export const deleteProduct = async (req, res) =>{
-    try {
-        const post = await Product.deleteOne({ _id: req.params.id });
-        res.json(post);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    
-}
+export const getProduct = async (req, res) => {
+  try {
+    const post = await Product.findById({ _id: req.params.id });
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-export const getProduct = async (req, res) =>{
-    try {
-        const post = await Product.findById({ _id: req.params.id });
-        res.json(post);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    
-}
+export const updateProduct = async (req, res) => {
+  try {
+    const post = await Product.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body
+    );
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-export const updateProduct = async (req, res) =>{
-    try {
-        
-        const post = await Product.findByIdAndUpdate({ _id: req.params.id }, req.body);
-        res.json(post);
+export const getProductSorter = async (req, res) => {
+  try {
+    const post = await Product.find().sort({ price: req.params.order });
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    
-}
+export const getProductFilter = async (req, res) => {
+  try {
+    const post = await Product.find(req.params);
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-export const getProductSorter = async (req, res) =>{
-    try {
-       
-        const post = await Product.find().sort({price : req.params.order});
-        res.json(post);
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    
-}
-
-export const getProductFilter = async (req, res) =>{
-    try {
-        
-        const post = await Product.find(req.params);
-        res.json(post);
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    
-}
-
-export const getProductBySize = async (req, res) =>{
-    try {
-        let size;
-       
-        const post = await Product.find({size : parseInt(req.params.size) || req.params.size });
-        res.json(post);
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-    
-}
+export const getProductBySize = async (req, res) => {
+  try {
+    const post = await Product.find({ size: req.params.size });
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
